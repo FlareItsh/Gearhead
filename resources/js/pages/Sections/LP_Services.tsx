92dd8ece2dec1d752d3986e05c2e7e089ffb230c
Flare <rayverdasalla@gmail.com>
2025-11-16 21:52:04 +0800

    Landing Page Formats

---
+++ resources/js/pages/Sections/LP_Services.tsx
@@ -0,0 +1,7 @@
+export default function LP_Services() {
+    return (
+        <>
+            <div></div>
+        </>
+    );
+}
--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,5 +1,10 @@
 import Header from '@/components/Header';
 import { Head } from '@inertiajs/react';
+import About from './Sections/About';
+import Footer from './Sections/Footer';
+import Hero from './Sections/Hero';
+import LP_Services from './Sections/LP_Services';
+import Review from './Sections/Review';
 
 export default function Welcome() {
     return (
@@ -12,6 +17,21 @@ export default function Welcome() {
                 />
             </Head>
             <Header />
+            <section>
+                <Hero></Hero>
+            </section>
+            <section>
+                <About></About>
+            </section>
+            <section>
+                <LP_Services></LP_Services>
+            </section>
+            <section>
+                <Review></Review>
+            </section>
+            <section>
+                <Footer></Footer>
+            </section>
         </>
     );
 }
