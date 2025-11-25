2bb858bbc7656da55171c4be9edc7f1680856cbb
Roseinashell <castrorochelle85@gmail.com>
2025-11-25 21:09:23 +0800

    Login

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -20,12 +20,20 @@ export default function AuthSplitLayout({
         <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
                 <div className="absolute inset-0 bg-zinc-900" />
+                <img
+                    src="login-Img.png"
+                    className="absolute inset-0 h-full w-full object-cover"
+                />
+                <div className="absolute inset-0 bg-black/20" />
+
                 <Link
                     href={home()}
                     className="relative z-20 flex items-center text-lg font-medium"
                 >
-                    <AppLogoIcon className="mr-2 size-8 fill-current text-white" />
-                    {name}
+                    <img
+                        src="/Gearhead-Logo-DarkMode.png"
+                        className="mr-2 h-10 w-auto"
+                    />
                 </Link>
                 {quote && (
                     <div className="relative z-20 mt-auto">
@@ -46,7 +54,10 @@ export default function AuthSplitLayout({
                         href={home()}
                         className="relative z-20 flex items-center justify-center lg:hidden"
                     >
-                        <AppLogoIcon className="h-10 fill-current text-black sm:h-12" />
+                        <img
+                            src="/Gearhead-Logo.png"
+                            className="mr-2 h-10 w-auto"
+                        />
                     </Link>
                     <div className="flex flex-col items-start gap-2 text-left sm:items-center sm:text-center">
                         <h1 className="text-xl font-medium">{title}</h1>
